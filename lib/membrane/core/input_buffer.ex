1361a325cf390951ff019d1459310f3d089da8bd
Mateusz Front <mateusz.front@swmansion.com>
2021-06-29 16:54:14 +0200

    first version of auto demands

--- lib/membrane/core/input_buffer.ex
+++ lib/membrane/core/input_buffer.ex
@@ -111,7 +111,8 @@ defmodule Membrane.Core.InputBuffer do
   end
 
   @spec enable_toilet(t()) :: t()
-  def enable_toilet(buf), do: %__MODULE__{buf | toilet?: true}
+  def enable_toilet(%__MODULE__{} = buf), do: %__MODULE__{buf | toilet?: true}
+  def enable_toilet(nil), do: nil
 
   @spec store(t(), atom(), any()) :: t()
   def store(input_buf, type \\ :buffers, v)
@@ -329,4 +330,5 @@ defmodule Membrane.Core.InputBuffer do
 
   @spec empty?(t()) :: boolean()
   def empty?(%__MODULE__{current_size: size}), do: size == 0
+  def empty?(nil), do: true
 end
